63f53591c3c8ac0c96286336b453986e9abdc78f
Alejandro <63469503+alejandro-mun@users.noreply.github.com>
2021-02-19 15:35:23 -0500

    Add files via upload
    Added toFixed to round down amount to 2 decimal places.

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App =()=>{
     setExpense("")   
     let expenseFloat = parseFloat(expense, 10)  //turn string into float
     setexpenseArray([...expenseArray, {id: Math.floor(Math.random() * 1001), title: title, expense: expenseFloat}])  //new array
-    findTotal()   //find the total
+    findTotal()  //find the total
   }
 const findTotal = ()=>{
   let total = 0
@@ -40,7 +40,11 @@ const findTotal = ()=>{
        acc +=curr.expense
       return acc
   }, 0)
-return total
+return total.toFixed(2) 
+}
+const difference = ()=>{
+  const difference = incomeArray-findTotal()
+  return difference.toFixed(2)
 }
   //console.log(expenseArray)
 
@@ -56,15 +60,14 @@ return total
           <h5>With every entry, the array's
             state is updated using React Hooks. Hover over the expense to see the array. The total of the expenses is then calculated using a reduce function.</h5>
           <h5>Lastly, the expense total is subracted
-          from the income entered and the difference is show in Left Over. This amount will be given a
-          class that makes it red or green depending on the if it's positive or negative using the ternary operator.
+          from the income entered and the difference is show in "Left Over". This amount will be given a
+          class that makes it red or green depending on if it's positive or negative using the ternary operator.
           </h5>
-
         </div>
           <div className="income section">        
             <form className="income-form">
               <h3>Enter your income</h3>
-              <input type="number" className="input-boxes" value={income} onChange={incomeHandler}/>
+              <input type="number" placeholder="1000" className="input-boxes" value={income} onChange={incomeHandler}/>
               <button onClick={submitIncome} className="button">Submit</button>
             </form>
           </div>
@@ -72,10 +75,10 @@ return total
             <form className="expense-form">
               <h3>Enter Expense</h3>
               <p>Expense Title</p>
-              <input type="text" className="input-boxes" value={title} onChange={titleHandler}/>
+              <input type="text" placeholder="Utility bill" className="input-boxes" value={title} onChange={titleHandler}/>
               <p>Amount </p>
-              <input type="number" className="input-boxes" value={expense} onChange={expenseHandler}/>
-              <p>Category</p>
+              <input type="number" placeholder="50" className="input-boxes" value={expense} onChange={expenseHandler}/>
+              <p>Category (not working yet)</p>
               <select className="input-boxes">
                 <option></option>
                 <option>Bills</option>
@@ -93,16 +96,17 @@ return total
           <div className="summary-bg">
           <h3>Summary</h3>
             <div className="summary">
-              <p>Income:<strong>${incomeArray}</strong></p>
-              <p>Expenses: <strong>${findTotal()}</strong></p>
-              <p>Left over: <span className={incomeArray-findTotal()> 0 ?'good': 'bad'}>${incomeArray-findTotal()}</span></p>
+              <h3 className="expense-item">Income: ${incomeArray}</h3>
+              <h3 className="expense-item">Expenses: ${findTotal()}</h3>
+              <h3 className="expense-item">Left over: <span className={incomeArray-findTotal()> 0 ?'good': 'bad'}>${difference()}</span></h3>
             </div>
           </div>          
         </div>
 
         <div className="expenses-section">
-          <h3>Expenses</h3>
+          
             <div className="expense-categories">
+            <h3>Expenses</h3>
               {expenseArray.map(expense=>{
                 return (
                   <div key={expense.id} className="tool-tip">
