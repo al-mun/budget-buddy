dc0cd5a3e5033332d1b65a552a18c3e52edb918a
Alejandro <63469503+alejandro-mun@users.noreply.github.com>
2021-05-10 18:42:09 -0400

    Add files via upload
    Added category selection check (cannot submit without choosing category).
    Added styling
    Added totals for each category

--- src/App.js
+++ src/App.js
@@ -7,17 +7,32 @@ import ExpenseList from './components/ExpenseList';
 
 const App =()=>{
   //useState initializations
+
+  //income section of app
   const [income, setIncome] = useState([])
   const [incomeArray, setIncomeArray] = useState([])
+
+  //enter an expense section
   const [title, setTitle] = useState("")
   const [expense, setExpense] = useState([])
   const [category, setCategory] = useState(0)
   const [expenseArray, setexpenseArray] = useState([])  //where the expense array is stored
 
+  //categories arrays
+  const [billsArray, setBillsArray] = useState([])
+  const [groceryArray, setGroceryArray] = useState([])
+  const [funArray, setFunArray] = useState([])
+  const [emergencyArray, setEmergencyArray] = useState([])
+  const [savingsArray, setSavingsArray] = useState([])
+
+  const [total, setTotal] = useState([])
+
   //functions
+  //set the income entered by the user as income
   const incomeHandler=(e)=>{
     setIncome(e.target.value)
   }
+
   const submitIncome=(e)=>{
     e.preventDefault()
     //input validation
@@ -26,9 +41,10 @@ const App =()=>{
     }
     else{
       setIncome("")
-      setIncomeArray(parseFloat(income, 10))
+      setIncomeArray(parseFloat(income, 10))  //parse income 
     }
   }
+  
   const titleHandler=(e)=>{
     setTitle(e.target.value)
   }
@@ -38,20 +54,49 @@ const App =()=>{
   const categoryHandler=(e)=>{
     setCategory(e.target.value)
   }
+
+  const categorizer=()=>{
+    
+    let cat = parseInt(category, 10)
+    let amount = parseFloat(expense)
+    if(cat === 1){
+      setBillsArray([...billsArray, {id: Math.floor(Math.random() * 1001), title: title, expense: amount, category: cat}])
+    }
+    else if(cat===2){
+      setGroceryArray([...groceryArray, {id: Math.floor(Math.random() * 1001), title: title, expense: amount, category: cat}])
+    }
+    else if(cat===3){
+      setFunArray([...funArray, {id: Math.floor(Math.random() * 1001), title: title, expense: amount, category: cat}])
+    }
+    else if(cat ===4){
+      setEmergencyArray([...emergencyArray, {id: Math.floor(Math.random() * 1001), title: title, expense: amount, category: cat}])
+    }
+    else if(cat ===5){
+      setSavingsArray([...savingsArray, {id: Math.floor(Math.random() * 1001), title: title, expense: amount, category: cat}])
+    }
+  }
   const submitExpense=(e)=>{
     e.preventDefault()  //stop refresh
     //input validation
-    if(title.length <=0 || expense.length <=0){
+    if(title.length <=0 || expense.length <=0 || category===0){
       alert("cannot be blank")
     }
     else{
       setTitle("")        //reset textboxes
-      setExpense("")
-      setCategory(0)   
+      setExpense("") 
       let expenseFloat = parseFloat(expense, 10)  //turn string into float
       let categoryInt = parseInt(category, 10)
       setexpenseArray([...expenseArray, {id: Math.floor(Math.random() * 1001), title: title, expense: expenseFloat, category: categoryInt}])  //new array
       findTotal()  //find the total
+    
+      setTotal([])
+      totalBills()
+      totalGrocery()
+      totalFun()
+      totalEmergency()
+      totalSavings()
+      categorizer(category)
+
     }
   }
   const findTotal = ()=>{
@@ -63,6 +108,52 @@ const App =()=>{
     }, 0)
   return total.toFixed(2)
   }
+  const totalBills=()=>{
+    let total = 0
+    //console.log(expenseArray.length)
+    total = billsArray.reduce(function(acc,curr){
+         acc +=curr.expense
+        return acc
+    }, 0)
+    return total.toFixed(2)
+  }
+  const totalGrocery=()=>{
+    let total = 0
+    //console.log(expenseArray.length)
+    total = groceryArray.reduce(function(acc,curr){
+         acc +=curr.expense
+        return acc
+    }, 0)
+    return total.toFixed(2)
+  }
+  const totalFun = ()=>{
+    let total = 0
+    //console.log(expenseArray.length)
+    total = funArray.reduce(function(acc,curr){
+         acc +=curr.expense
+        return acc
+    }, 0)
+    return total.toFixed(2)
+  }
+  const totalEmergency=()=>{
+    let total = 0
+    //console.log(expenseArray.length)
+    total = emergencyArray.reduce(function(acc,curr){
+         acc +=curr.expense
+        return acc
+    }, 0)
+    return total.toFixed(2)
+  }
+  const totalSavings=()=>{
+    let total = 0
+    //console.log(expenseArray.length)
+    total = savingsArray.reduce(function(acc,curr){
+         acc +=curr.expense
+        return acc
+    }, 0)
+    return total.toFixed(2)
+  }
+  
   const difference = ()=>{
     const difference = incomeArray-findTotal()
     return difference.toFixed(2)
@@ -103,9 +194,19 @@ const App =()=>{
           findTotal={findTotal()}
           difference={difference()}
           />
-        <ExpenseList 
+        <ExpenseList
+          totalBills={totalBills()}
+          totalGrocery={totalGrocery()}
+          totalFun={totalFun()}
+          totalEmergency={totalEmergency()}
+          totalSavings={totalSavings()}
+
+          bills={billsArray}
+          grocery={groceryArray}
+          fun={funArray}
+          emergency={emergencyArray}
+          savings={savingsArray}
           expense={expense}
-          title={title}
           expenseArray={expenseArray}
           category={category}/>
     </div>
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,31 +1,91 @@
 import React, { useState } from "react"
 
-const ExpenseList = ({title, expenseFloat, categoryInt, expenseArray, category})=>{
-
+const ExpenseList = ({bills, grocery,fun,emergency,
+    savings, totalBills, totalGrocery, totalFun, totalEmergency, totalSavings})=>{
+        //totalbills,totalGrocery,totalFun,totalEmergency,totalSavings
     //need a function or operator to have each bill entered added to its respective category (depending on user selection)
 
 
+  
     return(
         <div className="expenses-section">
-            <h3>Expenses</h3>
-
-            {expenseArray.map(expense=>{
-                    return (
-                        <div key={expense.id} className="tool-tip">
-                            <p className="heading">{expense.title} ${expense.expense.toFixed(2)}</p>
-                            <span className="tool-tip-info">&#123; id: {expense.id} title: {expense.title} amount: {expense.expense} category: {expense.category}&#125;</span>
-                        </div>
-                    )
-                })}
-                
+            <h1>Expenses</h1>
             <div className="expense-categories">
-                <h3>Bills</h3>
-                <h3>Grocery</h3>
-                <h3>Fun</h3>
-                <h3>Emergency</h3>
-                <h3>Savings</h3>
-
-
+                <div className="expense-items expense-categories">
+                    <div className="title">
+                        <h3>Bills</h3>
+                    </div>
+                    {bills.map(bill=>{
+                            return(
+                                <div key={bill.id} className="item-details">
+                                    <p>{bill.title}</p>
+                                    <p>${bill.expense}</p>        
+                                </div>
+                            )
+                        })}
+                    <p>Total: {totalBills}</p>
+                </div>
+                
+                <div className="expense-items expense-categories">
+                    <div className="title">
+                        <h3>Grocery</h3>
+                    </div>
+                    {grocery.map(grocery=>{
+                            return(
+                                <div key={grocery.id} className="item-details">
+                                    <p>{grocery.title}</p>
+                                    <p>${grocery.expense}</p>
+                                </div>
+                            )
+                        })}
+                    <p>Total: {totalGrocery}</p>
+                </div>
+                
+                <div className="expense-items expense-categories">
+                    <div className="title">
+                        <h3>Fun</h3>
+                    </div>
+                    {fun.map(fun=>{
+                            return(
+                                <div key={fun.id} className="item-details">
+                                    <p>{fun.title}</p>
+                                    <p>${fun.expense}</p>                          
+                                </div>
+                            )
+                        })}
+                    <p>Total: {totalFun}</p>
+                </div>
+ 
+                
+                <div className="expense-items expense-categories">
+                    <div className="title">
+                        <h3>Emergency</h3>
+                    </div>
+                    {emergency.map(emergency=>{
+                            return(
+                                <div key={emergency.id} className="item-details">
+                                    <p>{emergency.title}</p>
+                                    <p>${emergency.expense}</p>
+                                </div>
+                            )
+                        })}
+                    <p>Total: {totalEmergency} </p>
+                </div>
+                
+                <div className="expense-items expense-categories">
+                    <div className="title">
+                        <h3>Savings</h3>
+                    </div>
+                    {savings.map(savings=>{
+                            return(
+                                <div key={savings.id} className="item-details">
+                                    <p>{savings.title}</p>
+                                    <p>${savings.expense}</p>
+                                </div>
+                            )
+                        })}
+                    <p>Total: {totalSavings}</p>
+                </div>
             </div>
         </div>)
 }
