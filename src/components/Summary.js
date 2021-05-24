dc2c853db348118bcfc435d691db23ee064b1f91
Alejandro <63469503+alejandro-mun@users.noreply.github.com>
2021-05-24 12:20:30 -0400

    Add files via upload

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,18 +1,20 @@
 import React from "react"
 
-const Summary = (props)=>{
+const Summary = ({totalExpenses, incomeArray})=>{
+
+  const leftOver = incomeArray - totalExpenses
 
     return (
         <div className="summary-expenses">
         <div className="summary-bg">
         <h3>Summary</h3>
           <div className="summary">
-            <h3 className="heading">Income: $<span className="good">{props.incomeArray}</span></h3>
-            <h3 className="heading">Expenses: ${props.findTotal}</h3>
+            <h3 className="heading">Income: $<span className="good">{incomeArray}</span></h3>
+            <h3 className="heading">Expenses: ${totalExpenses}</h3>
             <h3 className="heading">Left over: $
                 <span 
-                className={props.incomeArray-props.findTotal > 0 ?'good': 'bad'}>
-                    {props.difference}
+                className={incomeArray-totalExpenses > 0 ?'good': 'bad'}>
+                    {leftOver.toFixed(2)}
                 </span>
             </h3>
           </div>
