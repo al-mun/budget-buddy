49fca6c0da73b90cae26304c40383958022d3820
Alejandro <63469503+alejandro-mun@users.noreply.github.com>
2021-06-08 22:39:02 -0400

    Add files via upload
    Testing modify function on bills only

--- src/components/Categories/Bills.js
+++ src/components/Categories/Bills.js
@@ -1,8 +1,16 @@
 import React, {useState} from "react"
 
-const Bills = ({title, bills, total, modifyItem, deleteItem, modifyOverlay, submitModify,titleHandler})=>{
+const Bills = ({title, bills, total, deleteItem, submitModify,titleHandler})=>{
 
+const [activeIndex, setActiveIndex]=useState(0)
 
+const modifyItem = (id)=>{
+    setActiveIndex(id)
+    console.log(id)
+}
+const cancel = ()=>{
+    setActiveIndex(0)
+}
 
     return(
         <div className="expense-items expense-categories">
@@ -11,32 +19,33 @@ const Bills = ({title, bills, total, modifyItem, deleteItem, modifyOverlay, subm
             </div>
             {bills.map(bill=>{
                     return(
-                        <div key={bill.id} className={modifyOverlay? "item-overlay": "item"}>
-                            <div className={modifyOverlay ? "modify-overlay": "modify-overlay-visible"}>                     
-                                <input type="text" value={bill.title} onChange={titleHandler} className="input-boxes"/>
-                                <input type="text" value={bill.expense}/>
-                                <select>
-                                    <option value="1">Bills</option>
-                                    <option value="2">Grocery/Household</option>
-                                    <option value="3">Fun</option>
-                                    <option value="4">Emergency</option>
-                                    <option value="5">Savings</option>
-                                </select>
+                        <div key={bill.id} className="item">
+                            <div className={activeIndex === bill.id ? "item-details-invisible": "item-details"}>
+                                <p>{bill.title}</p>
+                                <p>${bill.expense.toFixed(2)}</p>
+                                
                                 <div className="buttons">                                    
-                                    <button className="btn modify" onClick={()=>submitModify()}>Submit</button>
-                                    <button className="btn" onClick={()=>modifyItem(bill.id)}>Cancel</button>
+                                    <button className="btn modify" onClick={()=>modifyItem(bill.id)}>Modify</button>
+                                    <button className="btn" onClick={()=>deleteItem(bill.id)}>Delete</button> 
                                 </div>
+
                             </div>
+                                <div className={activeIndex === bill.id ? "item-details": "item-details-invisible"}>                     
+                                    <input type="text" value={bill.title} onChange={titleHandler} className="input-boxes"/>
+                                    <input type="text" value={bill.expense} onChange={titleHandler}/>
+                                    <select>
+                                        <option value="1">Bills</option>
+                                        <option value="2">Grocery/Household</option>
+                                        <option value="3">Fun</option>
+                                        <option value="4">Emergency</option>
+                                        <option value="5">Savings</option>
+                                    </select>
+                                    <div className="buttons">                                    
+                                        <button className="btn modify" onClick={()=>submitModify()}>Submit</button>
+                                        <button className="btn" key={bill.id} onClick={()=>cancel()}>Cancel</button>
+                                    </div>
+                                </div>
                             
-                            <p className={modifyOverlay? "item-details-bg" : "item-details"}>{bill.title}</p>
-                            <p className={modifyOverlay? "item-details-bg" : "item-details"}>${bill.expense.toFixed(2)}</p>
-
-                            <div className="buttons" className={modifyOverlay? "btns-invisible" : "btns"}>                                    
-                                <button className="btn modify" onClick={()=>modifyItem(bill.id)}>Modify</button>
-                                <button className="btn" onClick={()=>deleteItem(bill.id)}>Delete</button> 
-                            </div>
-
-
                         </div>
                     )
                 })}
